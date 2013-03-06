a663da52f3ea48e8a504d8999533814d1870e9fa
Eldrick Tobin <eldricktobin@gmail.com>
2013-03-06 14:24:47 -0600

    0.34.11

--- dfhack/scripts/movein.lua
+++
@@ -1,5 +0,0 @@
-dfhack.gui.getSelectedUnit().flags1.diplomat = 0
-dfhack.gui.getSelectedUnit().flags1.merchant = 0
-dfhack.gui.getSelectedUnit().flags1.forest = 0
-dfhack.gui.getSelectedUnit().flags2.resident = 0
-dfhack.gui.getSelectedUnit().civ_id = df.global.ui.civ_id
