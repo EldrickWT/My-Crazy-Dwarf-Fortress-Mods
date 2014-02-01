f1afcdb8cd04e64677f28eebc1bbb1c3f7655bfc
Eldrick Tobin <eldricktobin@gmail.com>
2014-01-31 23:47:35 -0600

    0.34.11 r4

--- dfhack/scripts/conscript.lua
+++ dfhack/scripts/conscript.lua
@@ -1,4 +1,5 @@
--- Turns babies and children into adults, and assigns all labors to everyone in the fortress -also checks for messed up labors, and taming.
+--Turns babies and children into adults, and assigns all labors to everyone in the fortress -also checks for messed up labors, and taming.
+--I know it doesn't look it but I am trying to not just tabulate every single creature in the objects directory. Really I swear.
 debug = false
 for _,arg in ipairs({...}) do
 	if string.lower(arg) == "debug" then debug = true	end
@@ -9,9 +10,12 @@ for _,v in ipairs(df.global.world.units.active) do
             v.profession = 102
             v.profession2 = 102
             if (v.flags1.diplomat == false)
-              and (v.flags1.merchant == false)
-              and (v.flags1.forest == false) then 
+              or (v.flags1.merchant == false)
+              or (v.flags1.forest == false) then
                 v.relations.following = nil
+                v.flags1.ridden=false
+                v.flags1.rider=false
+                v.relations.rider_mount_id=-1
             end
             if v.name.has_name == false then
                 namey = 'This one'
@@ -27,7 +31,7 @@ for _,v in ipairs(df.global.world.units.active) do
             namey2 = 'named ' ..dfhack.TranslateName(v.name,true)
         end
         -- We don't check for POWER, should we? We also don't get nitty and gritty about the entity races.
-        -- Training_level 9 is "Semi-Wild" when the tame flag is true. df.global.ui.civ_id members are typically level 9
+        -- Training_level 9 is -SOME KIND OF NORMAL- when the tame flag is false. df.global.ui.civ_id members are typically level 9
         if (df.global.world.raws.creatures.all[v.race].creature_id == "DOWNTRODDEN")
           or (df.global.world.raws.creatures.all[v.race].creature_id == "FABULOUSA")
           or (df.global.world.raws.creatures.all[v.race].creature_id == "FOOCUBI")
@@ -35,6 +39,7 @@ for _,v in ipairs(df.global.world.units.active) do
           or (df.global.world.raws.creatures.all[v.race].creature_id == "MEWLI")
           or (df.global.world.raws.creatures.all[v.race].creature_id == "MESSIANIC_FLUFFBALL")
           or (df.global.world.raws.creatures.all[v.race].creature_id == "QUEEN_SUCCUBUS")
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "TROLL")
           or (df.global.world.raws.creatures.all[v.race].creature_id == "WRAITH") then
             if (debug == true) then print("Untaming a " ..df.global.world.raws.creatures.all[v.race].creature_id.. " " ..namey2) end
             v.flags1.tame = false
@@ -47,6 +52,32 @@ for _,v in ipairs(df.global.world.units.active) do
             if (debug == true) then print("Impairment detected: " ..df.global.world.raws.creatures.all[v.race].creature_id.. " " ..namey2) end
             v.flags1.tame = true
             v.training_level = 7
+        --Need to spell out the higher order/entity races in case of "tweak makeown" and "mercenaries". Animalmen'll still be boned but they kinda should be. At current anyway.
+        --In either event, we're talking about CIV members already anyway. Aaand we already covered "pets" so now Mercenaries.
+        --Could potentially be cleaned up to pull from the master entity list or the entity raws ... but this should be quick at least.
+        elseif (df.global.world.raws.creatures.all[v.race].creature_id == "ANGEL") --ANGEL_CIV
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "SEPUTUS") --CULTIST
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "DWARF") --MOUNTAIN
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "ELF") --FOREST
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "HUMAN") --PLAINS
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "GOBLIN") --EVIL
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "KOBOLD") --SKULKING...
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "FABULOUSA") --FABULOUSA
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "FALLEN") --FALLEN
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "KAPA") --KAPA...
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "KOLCHA") --KOLCHA
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "LOLI") --LOLI_CIV
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "MEWLI") --MEWLI
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "MLA") --MLA_CIV
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "PEDO") --PEDO ... 
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "FOOCUBI") --W_FOOCUBI_CIV
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "WRAITH") --WRAITH
+          or (df.global.world.raws.creatures.all[v.race].creature_id == "YIFFIAN") then --YIFFIAN_CIV
+            if (debug == true) then print("Mercenary " ..df.global.world.raws.creatures.all[v.race].creature_id.. " " ..namey2) end
+            v.flags1.tame = false
+            v.training_level = 9
+        --Spot open for Semi-megabeasts here...
+        --Spot open for Megabeasts here...
         elseif not (v.race == df.global.ui.race_id) then
             if (debug == true) then print("Race doesn't match for a " ..df.global.world.raws.creatures.all[v.race].creature_id.. " " ..namey2) end
             v.flags1.tame = true
@@ -56,10 +87,7 @@ for _,v in ipairs(df.global.world.units.active) do
             v.flags1.tame = false
             v.training_level = 9
         end
-        v.flags1.ridden=false 
-        v.flags1.rider=false 
         v.flags1.on_ground=false 
-        v.relations.rider_mount_id=-1 
         if v.mood==8 then
             v.mood=-1
         end
--- dfhack/scripts/gen_check.lua
+++ dfhack/scripts/gen_check.lua
@@ -1,112 +1,61 @@
---Gender check. Just a gender check for the ACTIVE units. <-=--- I'LL REPURPOSE THIS LATER.
-everyone = false
-for _,arg in ipairs({...}) do
-	if arg == "all" or arg == "ALL" then everyone = true
-	else everyone = false
-	end
-end
-function gender_check ()
+--Gender check. Just a gender check for the selected unit. <-=--- I'LL LIKELY REPURPOSE LATER
+function gen_check ()
 	local unit, unit2, unit3, gendery, spousey2, spousey3, namey2, namey3, namey1done, namey2done, namey3done = ""
-	print(unit)
-	print(unit2)
-	print(unit3)
-	print(gendery)
-	print(spousey2)
-	print(spousey3)
-	print(namey2)
-	print(namey3)
-	print(namey1done)
-	print(namey2done)
-	print(namey3done)
 	unit=dfhack.gui.getSelectedUnit()
-	print(unit)
-		if (unit.civ_id == df.global.ui.civ_id) then
-	print(unit.civ_id)
-	print(df.global.ui.civ_id)
-			if not (unit.flags1.dead) then
-				if (df.global.world.raws.creatures.all[unit.race].caste[unit.caste].gender == 0) then
-					gendery = "Female [0]"
-	print(gendery)
-				elseif (df.global.world.raws.creatures.all[unit.race].caste[unit.caste].gender == 1) then
-					gendery = "Male [1]"
-	print(gendery)
-				else
-					gendery = "Other [cabbage]"
-	print(gendery)
-				end
-	print(unit.name.has_name)
-				if unit.name.has_name == false then
-					namey1done = df.global.world.raws.creatures.all[unit.race].name[0]
-	print(namey1done)
-				else
-					namey1done = dfhack.TranslateName(unit.name,false,false)
-	print(namey1done)
-				end
-	print(unit.relations.spouse_id)
-				if unit.relations.spouse_id == -1 then
-					spousey2 = "They are not married."
-	print(spousey2)
-				else
-					spousey2 = "They are married to "
-	print(spousey2)
-					for _,unit2 in ipairs(df.global.world.units.active) do
-						if (unit.id == unit2.relations.spouse_id) then
-	print(unit2.relations.spouse_id)
-							namey2 = unit2
-	print(namey2)
-	print(unit2)
+	if (unit.civ_id == df.global.ui.civ_id) then
+		if not (unit.flags1.dead) then
+			if (df.global.world.raws.creatures.all[unit.race].caste[unit.caste].gender == 0) then
+				gendery = "Female [0]"
+			elseif (df.global.world.raws.creatures.all[unit.race].caste[unit.caste].gender == 1) then
+				gendery = "Male [1]"
+			else
+				gendery = "Other [cabbage]"
+			end
+			if unit.name.has_name == false then
+				namey1done = df.global.world.raws.creatures.all[unit.race].name[0]
+			else
+				namey1done = dfhack.TranslateName(unit.name,false,false)
+			end
+			namey2done = " "
+			namey3done = " "
+			if unit.relations.spouse_id == -1 then
+				spousey2 = "They are not married."
+			else
+				spousey2 = "They are married to "
+				for _,unit2 in ipairs(df.global.world.units.active) do
+					if (unit.id == unit2.relations.spouse_id) then
+						namey2 = unit2.id
+						if unit2.name.has_name == false then
+							namey2done = df.global.world.raws.creatures.all[unit2.race].name[0]
+						else
+							namey2done = dfhack.TranslateName(unit2.name,false,false).."."
 						end
-					end
-				end
-				if unit.relations.lover_id == -1 then
-					spousey3 = "They are not busy Friday Night (wink)."
-	print(spousey3)
-				else
-					spousey3 = "They are busy Friday Night (wink) with "
-	print(spousey3)
-					for _,unit3 in ipairs(df.global.world.units.active) do
-						if (unit.id == unit3.relations.lover_id) then
-	print(unit3.relations.lover_id)
-							namey3 = unit3
-	print(namey3)
-	print(unit3)
-						end
-					end
-				end
-	print(namey2)
-				if not namey2 == "" then
-	print(namey2.has_name)
-					if namey2.has_name == false then
-						namey2done = df.global.world.raws.creatures.all[namey2.race].name[0]
-	print(namey2done)
-					else
-						namey2done = dfhack.TranslateName(namey2.name,false,false)
-	print(namey2done)
-					end
-				else
-					namey2done = ""
-				end
-	print(namey3)
-				if not namey3 == "" then
-	print(namey3.has_name)
-					if namey3.has_name == false then
-						namey3done = df.global.world.raws.creatures.all[namey3.race].name[0]
-	print(namey3done)
-					else
-						namey3done = dfhack.TranslateName(namey3.name,false,false)
-	print(namey3done)
-					end
-				else
-					namey3done = ""
-				end
-				if (everyone == true) then
-					print(namey1done.." is a "..df.global.world.raws.creatures.all[unit.race].creature_id.." ("..df.global.world.raws.creatures.all[unit.race].caste[unit.caste].caste_name[0]..") and their gender is "..gendery..". "..spousey2..""..namey2done.." "..spousey3..""..namey3done)
-				else
-					if not (namey2done == "") or not (namey3done == "") then
-						print(namey1done.." is a "..df.global.world.raws.creatures.all[unit.race].creature_id.." ("..df.global.world.raws.creatures.all[unit.race].caste[unit.caste].caste_name[0]..") and their gender is "..gendery..". "..spousey2..""..namey2done.." "..spousey3..""..namey3done)
 					end
 				end
 			end
+			if unit.relations.lover_id == -1 then
+				spousey3 = "They are not busy Friday Night (wink)."
+			else
+				spousey3 = "They are busy Friday Night (wink) with "
+				for _,unit3 in ipairs(df.global.world.units.active) do
+					if (unit.id == unit3.relations.lover_id) then
+						namey3 = unit3.id
+						if unit3.name.has_name == false then
+							namey3done = df.global.world.raws.creatures.all[unit3.race].name[0]
+						else
+							namey3done = dfhack.TranslateName(unit3.name,false,false).."."
+						end
+					end
+				end
+			end
+			if (namey2done == " ") then
+				namey2done = ""
+			end
+			if (namey3done == " ") then
+				namey3done = ""
+			end
+			print(namey1done.." is a "..df.global.world.raws.creatures.all[unit.race].creature_id.." ("..df.global.world.raws.creatures.all[unit.race].caste[unit.caste].caste_name[0]..") and their gender is "..gendery..". "..spousey2..""..namey2done.." "..spousey3..""..namey3done)
 		end
 	end
-dfhack.with_suspend(gender_check)
+end
+dfhack.with_suspend(gen_check)
